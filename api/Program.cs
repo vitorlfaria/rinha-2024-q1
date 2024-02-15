8a94363eda26356bfef2be82fea95faab212acc2
Vitor Lacerda <vitor.faria@grupobarigui.com.br>
2024-02-15 16:02:45 -0300

    feat: adiciona base dos endpoints

--- api/Program.cs
+++ api/Program.cs
@@ -1,12 +1,43 @@
+using rinha_2024_q1.Entities;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
-app.MapGet("/healthcheck", () =>
-{
-    return "Hello World!";
+app.MapGet("/healthcheck", () => "Working, thanks!");
+
+app.MapPost("clientes/{id}/transacoes", (int id, TransacaoRequest transacaorequest) => {
+    try
+    {
+        transacaorequest.Validate();
+        var transacaoResponse = new TransacaoResponse(1000, -2345);
+        return Results.Ok(transacaoResponse);
+    }
+    catch (Exception e)
+    {
+        return Results.UnprocessableEntity(e.Message);
+    }
+});
+
+app.MapGet("clientes/{id}/extrato", (int id) => {
+    var extrato = new Extrato
+    {
+        Saldo = new Saldo
+        {
+            Total = 1000,
+            Data_Extrato = DateTime.Now,
+            Limite = 5000
+        },
+        Ultimas_transacoes = new List<TransacaoDto>
+        {
+            new(1000, "credito", "Descrição", DateTime.Now),
+            new(-2345, "debito", "Descrição", DateTime.Now)
+        }
+    };
+
+    return Results.Ok(extrato);
 });
 
 app.Run();
